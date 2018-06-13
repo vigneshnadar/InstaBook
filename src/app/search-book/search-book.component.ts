b70c3f38ef566b35d155e63a507c4fa1ccbdc10c
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-13 17:54:26 -0400

    google api working

--- src/app/search-book/search-book.component.ts
+++ src/app/search-book/search-book.component.ts
@@ -19,53 +19,12 @@ export class SearchBookComponent implements OnInit {
     console.log(book);
 
     fetch
-    ('https://www.goodreads.com/search/index.xml?key=Rf5RgIMAFlMe3bqgBWQrQ&q=titan',{
-      headers: {
-       // 'Content-Type': 'text/xml',
-        'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'HEAD, GET, POST, PUT, PATCH, DELETE',
-   'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
-      }
-      //mode: 'no-cors'
-    })
-      .then(output => {
-        console.log(output.text());
-        return output.text();
-      })
-      .then( xmltext => {
-        console.log('xml text is');
-        console.log(xmltext);
-        parseString(xmltext, (err, goodreadsResult) => {
-            const op = {
-              books: goodreadsResult.GoodreadsResponse.search[0].result[0].work.map(
-                work => ({
-                  gdId: work.best_book[0].id[0]._,
-                  title: work.best_book[0].title[0],
-                  authors: work.best_book[0].author[0].name[0],
-                  covers: [work.best_book[0].image_url[0]]
-                })
-              )
-            }
-            console.log(op);
-          }
-        )
-      }
+    ('https://www.googleapis.com/books/v1/volumes?q=php')
+      .then(response => response.json())
+      .then(booklist => {
+        console.log(booklist);
       });
-
-    // // https://www.goodreads.com/search/index.xml?key=Rf5RgIMAFlMe3bqgBWQrQ&q=titan
   }
-
-  // addCourse(title) {
-  //   this.courses.push({
-  //     title: title,
-  //     id: title
-  //   });
-  //   this.courseTitle = '';
-  // }
-
-
-
-
   // findAllCourses() {
   //   return fetch
   //   ('http://localhost:8080/api/course')

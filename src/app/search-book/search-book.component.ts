d7cd556bf43ea36854ab767a231c3ea1d01e5c3c
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-13 17:50:34 -0400

    backup not working

--- src/app/search-book/search-book.component.ts
+++ src/app/search-book/search-book.component.ts
@@ -20,11 +20,37 @@ export class SearchBookComponent implements OnInit {
 
     fetch
     ('https://www.goodreads.com/search/index.xml?key=Rf5RgIMAFlMe3bqgBWQrQ&q=titan',{
-      credentials: 'same-origin',
-      mode: 'no-cors'
+      headers: {
+       // 'Content-Type': 'text/xml',
+        'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'HEAD, GET, POST, PUT, PATCH, DELETE',
+   'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+      }
+      //mode: 'no-cors'
     })
-      .then(response => response.json())
-      .then(booklist => console.log(booklist));
+      .then(output => {
+        console.log(output.text());
+        return output.text();
+      })
+      .then( xmltext => {
+        console.log('xml text is');
+        console.log(xmltext);
+        parseString(xmltext, (err, goodreadsResult) => {
+            const op = {
+              books: goodreadsResult.GoodreadsResponse.search[0].result[0].work.map(
+                work => ({
+                  gdId: work.best_book[0].id[0]._,
+                  title: work.best_book[0].title[0],
+                  authors: work.best_book[0].author[0].name[0],
+                  covers: [work.best_book[0].image_url[0]]
+                })
+              )
+            }
+            console.log(op);
+          }
+        )
+      }
+      });
 
     // // https://www.goodreads.com/search/index.xml?key=Rf5RgIMAFlMe3bqgBWQrQ&q=titan
   }

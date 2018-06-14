cdbbc4b083455181ff019f2eceb168feac11ca4e
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-14 11:53:06 -0400

    modified server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/InstaBook'));
+app.use(express.static('./src'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('./dist/InstaBook/index.html'));
+  res.sendFile(path.join('./src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

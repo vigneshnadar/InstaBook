31f983ae16972bfb06d2b9a16f0badba79b9e901
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-25 10:48:16 -0400

    top navbar

--- src/app/top-navbar/top-navbar.component.ts
+++ src/app/top-navbar/top-navbar.component.ts
@@ -50,4 +50,12 @@ export class TopNavbarComponent implements OnInit {
     this.router.navigate(['author']);
   }
 
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
+
 }

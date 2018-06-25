ea160c27cc788e073c2ba6a2f6d5e88c1a6f8830
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-25 10:10:34 -0400

    fixed review functionality

--- src/app/search-book/search-book.component.ts
+++ src/app/search-book/search-book.component.ts
@@ -50,32 +50,71 @@ export class SearchBookComponent implements OnInit {
     console.log('currentbook');
     console.log(this.currentBook);
     this.bookMarkVisible = true;
+
+    this.reviews = [];
+    this.bookService.findBookByTitle(book.volumeInfo.title)
+      .then(cbe => {
+        this.bookService.findReviewsForBook(cbe[0]._id)
+          .then(reviews => {
+            console.log('review');
+            console.log(reviews);
+            this.reviews = reviews;
+          });
+      });
   }
 
   // app.get('/api/book/:bookId/review', findReviewsForBook)
   // app.post('/api/:bookId/review', addReview)
 
   addReview(currentReview, cb) {
-    const newBook = {
-      name: cb.volumeInfo.title,
-      description: cb.searchInfo.textSnippet,
-      imageurl: cb.volumeInfo.imageLinks.thumbnail
-    }
-    console.log(newBook);
-    // alert(section._id);
-    this.bookService.createBook(newBook)
-      .then(createdBook => {
-        console.log(createdBook);
-        this.bookService.addReview(createdBook._id, currentReview)
-          .then(response => {
-            this.bookService.findReviewsForBook(createdBook._id)
-              .then(reviews => {
-                console.log('review');
-                console.log(reviews);
-                this.reviews = reviews;
+
+
+
+    if (this.user.username === 'unregistered') {
+      alert('Please register/login to bookmark');
+    } else {
+      const newBook = {
+        name: cb.volumeInfo.title,
+        description: cb.searchInfo.textSnippet,
+        imageurl: cb.volumeInfo.imageLinks.thumbnail
+      }
+      console.log(newBook);
+      this.bookService.findBookByTitle(cb.volumeInfo.title)
+        .then(cbe => {
+          console.log('book by title');
+          // console.log(createdBook);
+          if (cbe.length === 0) {
+            console.log('not found');
+
+            this.bookService.createBook(newBook)
+              .then(createdBook => {
+                console.log(createdBook);
+                this.bookService.addReview(createdBook._id, currentReview)
+                  .then(response => {
+                    this.bookService.findReviewsForBook(createdBook._id)
+                      .then(reviews => {
+                        console.log('review');
+                        console.log(reviews);
+                        this.reviews = reviews;
+                      });
+                  });
               });
-          });
-      });
+          } else {
+            console.log('book found');
+            console.log(cbe);
+            console.log(cbe[0]._id);
+            this.bookService.addReview(cbe[0]._id, currentReview)
+              .then(response => {
+                this.bookService.findReviewsForBook(cbe[0]._id)
+                  .then(reviews => {
+                    console.log('review');
+                    console.log(reviews);
+                    this.reviews = reviews;
+                  });
+              });
+          }
+        });
+    }
   }
 
 
@@ -111,13 +150,7 @@ export class SearchBookComponent implements OnInit {
                 console.log(cb[0]._id);
                 this.bookService.bookmarkUserInBook(cb[0]._id);
               }
-              // this.bookService.bookmarkUserInBook(createdBook._id);
             });
-      // this.bookService.createBook(newBook)
-      //   .then(createdBook => {
-      //     console.log(createdBook);
-      //     this.bookService.bookmarkUserInBook(createdBook._id);
-      //   });
     }
 
 
